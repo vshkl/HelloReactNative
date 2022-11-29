16939f56319002c670484fe8d914757156fa9e56
Pavel Vashkel <p.vashkel@datarockets.com>
2022-11-29 13:16:28 +0100

    Adjust code style and tidy-up js part

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-};
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        semi: ['error', 'never'],
+      },
+    },
+  ],
+}
--- app/App.js
+++ app/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React from 'react'
+import {StyleSheet, View} from 'react-native'
 
-import HelloWorld from './HelloWorld';
+import HelloWorld from './HelloWorld'
 
 const App = () => {
   return (
     <View style={styles.container}>
       <HelloWorld style={styles.helloWorld} />
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -21,6 +21,6 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
   },
-});
+})
 
-export default App;
+export default App
